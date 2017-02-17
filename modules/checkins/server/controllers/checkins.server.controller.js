78208b343b570c7879667447fb9fc27ecd3522e2
thammaratrud <thammarat.rud@gmail.com>
2017-02-17 10:17:05 +0700

    aaa

--- modules/checkins/server/controllers/checkins.server.controller.js
+++ modules/checkins/server/controllers/checkins.server.controller.js
@@ -81,7 +81,7 @@ exports.delete = function (req, res) {
  * List of Checkins
  */
 exports.list = function (req, res) {
-  Checkin.find().sort('-created').populate('user').exec(function (err, checkins) {
+  Checkin.find().sort('-created').populate('user', 'displayName').populate('user', 'profileImageURL').exec(function (err, checkins) {
     if (err) {
       return res.status(400).send({
         message: errorHandler.getErrorMessage(err)
